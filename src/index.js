056600d438ed1e1b77b1cee28c20d02afde5d503
Yusadolat <yusadolat@gmail.com>
2019-12-19 08:22:14 +0100

    Data With props :rocket:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-import Hello from './Hello';
+import Card from './Card';
+import { robots } from './robots'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello greeting={`Hello From Props!`} />, document.getElementById('root'));
+ReactDOM.render(<Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
